26dfd4a8546a789806a9aff9c8a724f4261d5561
mftech <matheussilvafeitosa78@gmail.com>
2023-05-28 01:58:34 -0300

    Data protection

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,20 +1,31 @@
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
+// Adicionando proteção de dados com DataProtection
+builder.Services.AddDataProtection();
+
 var app = builder.Build();
 
-app.MapGet("/username", (HttpContext ctx) =>
+app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
 {
     // Acessando Cookies do client
+    var protector = idp.CreateProtector("auth-cookie");
     var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-    var payload = authCookie.Split("=");
-    var key = payload.First();
-    var value = payload.Last();
+    var protectedPayload = authCookie.Split("=").Last();
+    // Unprotect discriptografamos o cookie no servidor e conseguimos fazer as devidas validações
+    var payload = protector.Unprotect(protectedPayload);
+    var parts = payload.Split(":");
+    var key = parts.First();
+    var value = parts.Last();
     return value;
 });
 
-app.MapGet("/login", (HttpContext ctx) =>
+app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
 {
+    var protector = idp.CreateProtector("auth-cookie");
     // Criamos um cookie no client, onde posteriormente poderemos acessar para obter informações
-    ctx.Response.Headers["set-cookie"] = "auth=usr:matheus";
+    // Protect criptografa os dados, dessa forma o que está sendo salvo neste cookie fica embaralhado no client
+    ctx.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:matheus")}";
     return "ok";
 });
 
