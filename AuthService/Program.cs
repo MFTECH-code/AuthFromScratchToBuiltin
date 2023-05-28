de4a031e1255d8215f21f8354625e678f9d3aec5
mftech <matheussilvafeitosa78@gmail.com>
2023-05-28 01:45:49 -0300

    Adjust auth cookie value and spliting payloand which key and value

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -5,13 +5,16 @@ app.MapGet("/username", (HttpContext ctx) =>
 {
     // Acessando Cookies do client
     var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-    return authCookie.Split("=").Last();
+    var payload = authCookie.Split("=");
+    var key = payload.First();
+    var value = payload.Last();
+    return value;
 });
 
 app.MapGet("/login", (HttpContext ctx) =>
 {
     // Criamos um cookie no client, onde posteriormente poderemos acessar para obter informações
-    ctx.Response.Headers["set-cookie"] = "auth=usr=matheus";
+    ctx.Response.Headers["set-cookie"] = "auth=usr:matheus";
     return "ok";
 });
 
